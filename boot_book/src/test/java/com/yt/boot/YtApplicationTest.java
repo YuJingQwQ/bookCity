018748b28329942b3bc3c3dd6c2995d74b0fd9e1
hyt <951734771@qq.com>
2021-08-15 21:01:20 +0800

    wuwu1

---
+++ boot_book/src/test/java/com/yt/boot/YtApplicationTest.java
@@ -0,0 +1,16 @@
+package com.yt.boot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+@description:
+@author Mr.He
+@create 2021-08-15 21:00
+*/
+class YtApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
